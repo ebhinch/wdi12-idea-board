4780a87e1fad3321437cf873163b06901ad7cf67
ebhinch <erica.hinchman@gmail.com>
2017-10-12 11:19:28 -0400

    setup express client folder and added in concurrently

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/", (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
 })
