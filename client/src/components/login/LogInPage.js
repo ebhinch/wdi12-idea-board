25626d50b92ce3635c44cef435d692e594d97503
ebhinch <erica.hinchman@gmail.com>
2017-10-12 15:33:30 -0400

    built out UsersController.js and used postman to test POST

--- client/src/components/login/LogInPage.js
+++ client/src/components/login/LogInPage.js
@@ -1,11 +1,37 @@
+//import things that come from node modules first
 import React, { Component } from 'react';
+import axios from "axios"
+
+//import other components next
+import { Link } from "react-router-dom";
+import SignUpForm from "./SignUpForm"
+
 
 class LoginPage extends Component {
+    state = {
+        users: []
+    }
+
+//we want to get API before anything renders, so we use componentWillMount
+componentWillMount () {
+    this.getAllUsers()
+}
+
+getAllUsers = async () => {
+    //this will return a promise
+    const response = await axios.get("/api/users")
+    this.setState({users: response.data})
+}
     render() {
         return (
             <div>
-                <h1>Hello LogIn Page</h1>
-            </div>
+            <h1>Log-In</h1>
+            <h3>Please Select an Existing User</h3>
+            {this.state.users.map(user => {
+              return (<Link key={user._id} to={`/idea/${user._id}`}>{user.userName}</Link>)
+            })}
+            <SignUpForm />
+          </div>
         );
     }
 }
--- routes/UsersController.js
+++ routes/UsersController.js
@@ -5,15 +5,29 @@ const { UserModel } = require("../db/schema")
 
 //use async/await instead of .then .catch promises
 router.get("/", async (request, response) => {
-    try{
-    const users = await UserModel.find({})
-    //will send back json
-    response.json(users)
+    try {
+        const users = await UserModel.find({})
+        //will send back json
+        response.json(users)
     }
     catch (error) {
         response.send(error)
     }
 })
 
+router.post("/", async (request, response) => {
+    try {
+    //create new instance of user
+    const newUser = new UserModel(request.body.user)
+    //then save
+    const saved = await newUser.save()
+    response.json(saved)
+    }
+    catch (error) {
+        response.send(error)
+    }
+}
+
+)
 
 module.exports = router
